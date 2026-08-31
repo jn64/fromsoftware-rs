ed39e52bf265e4ad4e72604b4bd36e7dde45d411
Justin Koh <j@ustink.org>
2026-08-31 13:26:52 +0800

    Copy spawn-asset example

---
+++ mods/portable-chair/src/lib.rs
@@ -0,0 +1,63 @@
+use std::time::Duration;
+
+use eldenring::{
+    cs::{
+        CSTaskGroupIndex, CSTaskImp, CSWorldGeomMan, ChrInsExt, GeometrySpawnParameters,
+        WorldChrMan,
+    },
+    fd4::FD4TaskData,
+    util::input,
+};
+use fromsoftware_shared::{FromStatic, SharedTaskImpExt};
+
+#[unsafe(no_mangle)]
+/// # Safety
+///
+/// This is exposed this way such that windows LoadLibrary API can call it. Do not call this yourself.
+pub unsafe extern "C" fn DllMain(_hmodule: usize, reason: u32) -> bool {
+    if reason != 1 {
+        return true;
+    }
+
+    // Kick off new thread.
+    std::thread::spawn(|| {
+        let cs_task = CSTaskImp::wait_for_instance(Duration::MAX).unwrap();
+        cs_task.run_recurring(
+            |_: &FD4TaskData| {
+                if !input::is_key_pressed(0x48) {
+                    return;
+                }
+
+                let Some(player) = unsafe { WorldChrMan::instance() }
+                    .ok()
+                    .and_then(|w| w.main_player.as_ref())
+                else {
+                    return;
+                };
+
+                let Some(block_geom_data) = unsafe { CSWorldGeomMan::instance_mut() }
+                    .ok()
+                    .and_then(|wgm| wgm.geom_block_data_by_id_mut(&player.chr_ins.block_id()))
+                else {
+                    return;
+                };
+
+                block_geom_data.spawn_geometry(
+                    "AEG099_831",
+                    &GeometrySpawnParameters {
+                        position: player.block_position,
+                        rot_x: 0.0,
+                        rot_y: 0.0,
+                        rot_z: 0.0,
+                        scale_x: 2.0,
+                        scale_y: 2.0,
+                        scale_z: 2.0,
+                    },
+                );
+            },
+            CSTaskGroupIndex::ChrIns_PostPhysics,
+        );
+    });
+
+    true
+}
